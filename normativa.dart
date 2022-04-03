442eb8b67f9db0cf6aac490d2e5ae873616d0682
Vickiornelas27 <102771422+Vickiornelas27@users.noreply.github.com>
2022-04-03 00:30:33 -0700

    normatividad

---
+++ normativa.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:vinculacion/api/pdfApi.dart';
+import 'package:vinculacion/page/PdfViewer.dart';
+
+class Normativa extends StatelessWidget {
+  const Normativa({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+            margin: const EdgeInsets.all(12),
+            child: Text('Normatividad',
+              style: TextStyle(
+                  fontSize: 26,
+                color: Colors.indigo[900],
+                fontWeight: FontWeight.bold
+              ),
+            )
+        ),
+
+        Container(
+            margin: const EdgeInsets.all(10),
+            child:
+            InkWell(
+              onTap: () async{
+                const path = 'assets/pdfs/presentacion.pdf';
+                final file = await PdfApi.loadAsset(path);
+                openPdf(context,file);
+              },
+              child: const Text('• Modelo de Educación Dual para nivel licenciatura del Tecnológico Nacional de México (MEDTecNM)',
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                    color: Colors.indigo,
+                ),
+              ),
+            ),
+
+        ),
+
+
+      ],
+    );
+  }
+
+  void openPdf(BuildContext context, File file) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => PdfViewerPage(file: file)));
+  }
+}
