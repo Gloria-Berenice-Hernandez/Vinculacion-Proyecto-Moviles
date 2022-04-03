332be8a7428140931a62f22eef8b2659fcdf49f0
Francisco-Adan-Martinez-Alejandre <al18760460@ite.edu.mx>
2022-04-03 12:31:05 -0700

    Formato cambio
    Textos

--- vinculacion/lib/page/pagina_modelo_dual.dart
+++ vinculacion/lib/page/pagina_modelo_dual.dart
@@ -30,7 +30,12 @@ class ModeloEducacionDual extends StatelessWidget {
           Container(
             margin: const EdgeInsets.all(10),
             child: const Text(
-              'Estudiantes del Instituto Tecnológico de Ensenada del TecNM.\n\nConoce e incorporate al Modelo de Educación Dual es importante en tu formación académica-laboral.\n\nTe presentamos Lineamientos, procedimiento, formatos y lo que necesitas conocer para inicia, dar seguimiento y concluir tu Modelo de Educación Dual.\n\nSi estas en tiempo para iniciarlo, empieza ya, no lo dejes para el último momento.',
+              'Estudiantes del Instituto Tecnológico de Ensenada del TecNM.\n\n'
+              'Conoce e incorporate al Modelo de Educación Dual es importante '
+              'en tu formación académica-laboral.\n\nTe presentamos Lineamientos, '
+              'procedimiento, formatos y lo que necesitas conocer para inicia, dar '
+              'seguimiento y concluir tu Modelo de Educación Dual.\n\nSi estas en '
+              'tiempo para iniciarlo, empieza ya, no lo dejes para el último momento.',
               textAlign: TextAlign.justify,
               style: TextStyle(
                 fontSize: 17,
@@ -53,28 +58,40 @@ class ModeloEducacionDual extends StatelessWidget {
           ),
           Convocatoria(
               imagen: 'assets/images/berrymexlogocompleto.png',
-              texto:
-                  'Empresa: BERRYMEX, S. DE R.L. DE C.V.\n\nEstudiante de: Ingenieria Industrial\n\nObjetivo del Proyecto: Debido a que se cuenta con el Objetivo de contar con la Documentación que permita la estandarización de los procesos y la calidad del mismo.',
+              texto: 'Empresa: BERRYMEX, S. DE R.L. DE C.V.\n\n'
+                  'Estudiante de: Ingenieria Industrial\n\n'
+                  'Objetivo del Proyecto: Debido a que se cuenta '
+                  'con el Objetivo de contar con la Documentación '
+                  'que permita la estandarización de los procesos y la calidad del mismo.',
               pdf: 'assets/pdfs/Practicante-ITE.pdf'),
           Convocatoria(
               imagen: 'assets/images/berrymexlogocompleto.png',
-              texto:
-                  'Empresa: BERRYMEX, S. DE R.L. DE C.V.\n\nEstudiante de la carrera: Ingeniería en Industrial - IGE - Administración\n\nObjetivo del Proyecto: Debido a que se cuenta con el Objetivo de contar con la Documentación que permita la estandarización de los procesos y la calidad del mismo.',
+              texto: 'Empresa: BERRYMEX, S. DE R.L. DE C.V.\n\n'
+                  'Estudiante de la carrera: Ingeniería en Industrial - IGE - Administración\n\n'
+                  'Objetivo del Proyecto: Debido a que se cuenta con el Objetivo de contar con la '
+                  'Documentación que permita la estandarización de los procesos y la calidad del mismo.',
               pdf: 'assets/pdfs/Solicitud-de-Estudiante-DUAL-RH.pdf'),
           Convocatoria(
               imagen: 'assets/images/berrymexlogocompleto.png',
-              texto:
-                  'Empresa: BERRYMEX, S. DE R.L. DE C.V.\n\nEstudiante de la carrera: Ingeniería en Industrial - IGE\n\nObjetivo del Proyecto: Debido a que se cuenta con el Objetivo de contar con la Documentación que permita la estandarización de los procesos y la calidad del mismo.',
+              texto: 'Empresa: BERRYMEX, S. DE R.L. DE C.V.\n\n'
+                  'Estudiante de la carrera: Ingeniería en Industrial - IGE\n\n'
+                  'Objetivo del Proyecto: Debido a que se cuenta con '
+                  'el Objetivo de contar con la Documentación que permita '
+                  'la estandarización de los procesos y la calidad del mismo.',
               pdf: 'assets/pdfs/Solicitud-Berrymex.pdf'),
           Convocatoria(
               imagen: 'assets/images/masprint.jpg',
-              texto:
-                  'Empresa: MAS PRINT\n\nEstudiante de la carrera: Ingeniería en Industrial - Administración\n\nObjetivo del Proyecto: Debido a que se cuenta con el Objetivo de contar con la Documentación que permita la estandarización de los procesos y la calidad del mismo.',
+              texto: 'Empresa: MAS PRINT\n\n'
+                  'Estudiante de la carrera: Ingeniería en Industrial - Administración\n\n'
+                  'Objetivo del Proyecto: Debido a que se cuenta con el Objetivo de contar '
+                  'con la Documentación que permita la estandarización de los procesos y la calidad del mismo.',
               pdf: 'assets/pdfs/SolicitudMasPrint.pdf'),
           Convocatoria(
               imagen: 'assets/images/masprint.jpg',
-              texto:
-                  'Empresa: MAS PRINT\n\nEstudiante de la carrera: Ingeniería en Gestión Empresarial - Administración\n\nObjetivo del Proyecto: Debido a que se cuenta con el Objetivo de contar con la Documentación que permita la estandarización de los procesos y la calidad del mismo.',
+              texto: 'Empresa: MAS PRINT\n\n'
+                  'Estudiante de la carrera: Ingeniería en Gestión Empresarial - Administración\n\n'
+                  'Objetivo del Proyecto: Debido a que se cuenta con el Objetivo de contar con la Do'
+                  'cumentación que permita la estandarización de los procesos y la calidad del mismo.',
               pdf: 'assets/pdfs/Solicitud-MAS-PRINT-Facturacion.pdf'),
           const Normativa(),
         ],
