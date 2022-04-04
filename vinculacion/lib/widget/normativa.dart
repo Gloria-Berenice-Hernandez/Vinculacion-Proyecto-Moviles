c647c5a823639a76470947a946d666e7adabf868
Francisco-Adan-Martinez-Alejandre <al18760460@ite.edu.mx>
2022-04-04 15:47:04 -0700

    Librerias

--- vinculacion/lib/widget/normativa.dart
+++ vinculacion/lib/widget/normativa.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:vinculacion/api/pdfApi.dart';
-import 'package:vinculacion/page/PdfViewer.dart';
+import 'package:vinculacion/api/pdf_api.dart';
+import 'package:vinculacion/page/pdf_viewer.dart';
 
 class Normativa extends StatelessWidget {
   const Normativa({Key? key}) : super(key: key);
@@ -13,40 +13,36 @@ class Normativa extends StatelessWidget {
       children: [
         Container(
             margin: const EdgeInsets.all(12),
-            child: Text('Normatividad',
+            child: Text(
+              'Normatividad',
               style: TextStyle(
                   fontSize: 26,
-                color: Colors.indigo[900],
-                fontWeight: FontWeight.bold
-              ),
-            )
-        ),
-
+                  color: Colors.indigo[900],
+                  fontWeight: FontWeight.bold),
+            )),
         Container(
-            margin: const EdgeInsets.all(10),
-            child:
-            InkWell(
-              onTap: () async{
-                const path = 'assets/pdfs/presentacion.pdf';
-                final file = await PdfApi.loadAsset(path);
-                openPdf(context,file);
-              },
-              child: const Text('• Modelo de Educación Dual para nivel licenciatura del Tecnológico Nacional de México (MEDTecNM)',
-                textAlign: TextAlign.justify,
-                style: TextStyle(
-                    color: Colors.indigo,
-                ),
+          margin: const EdgeInsets.all(10),
+          child: InkWell(
+            onTap: () async {
+              const path = 'assets/pdfs/presentacion.pdf';
+              final file = await PdfApi.loadAsset(path);
+              openPdf(context, file);
+            },
+            child: const Text(
+              '• Modelo de Educación Dual para nivel licenciatura del Tecnológico Nacional de México (MEDTecNM)',
+              textAlign: TextAlign.justify,
+              style: TextStyle(
+                color: Colors.indigo,
               ),
             ),
-
+          ),
         ),
-
-
       ],
     );
   }
 
   void openPdf(BuildContext context, File file) {
-    Navigator.of(context).push(MaterialPageRoute(builder: (context) => PdfViewerPage(file: file)));
+    Navigator.of(context).push(
+        MaterialPageRoute(builder: (context) => PdfViewerPage(file: file)));
   }
 }
